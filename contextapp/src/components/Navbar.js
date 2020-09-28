2c96f17b23bde433edffb5a77ab476c75356c055
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-28 00:27:41 -0400

    Accessing Context P.1

--- contextapp/src/components/Navbar.js
+++ contextapp/src/components/Navbar.js
@@ -1,9 +1,16 @@
 import React, { Component} from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+    //whatever context we want to consume INSIDE this componet
+    static contextType = ThemeContext;
+    //look up the cmonp tree, the first time it finds a provider for this context.
     render() { 
+        console.log(this.context)
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
         return (
-            <nav>
+            <nav style={{background: theme.ui, color: theme.syntax}}>
                 <h1>Context App</h1>
                 <ul>
                     <li>Home</li>
