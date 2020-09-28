4563b3eb7f62e7a8aefb127f7157c7294ab68230
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-09-28 17:05:11 -0400

    Intro to Hooks

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SongList = () => {
+    return ( 
+        <div className='song-list'>
+            <ul>
+                <li>time flies</li>
+                <li>let it go</li>
+            </ul>
+        </div>
+     );
+}
+ 
+export default SongList;
